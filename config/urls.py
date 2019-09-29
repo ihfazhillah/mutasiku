1d1b162c43f0f2d2809cae5720231a44720ec01a
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-09-29 22:41:44 +0700

    init template

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
         "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
     ),
     # Django Admin, use {% url 'admin:index' %}
+    path("dashboard-test/", TemplateView.as_view(template_name="base.html")),
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("mutasiku.users.urls", namespace="users")),
